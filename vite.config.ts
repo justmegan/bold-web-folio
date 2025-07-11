fb37cbbd8bdc73f969de3902e68f3ec41e6567c3
Aulia Mega <uh92t4@inf.elte.hu>
2025-07-11 12:29:23 +0200

    Add auto-deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/bold-web-folio/',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),   // <— alias @ -> /src
+      '@': resolve(__dirname, 'src'),
     },
   },
-});
+})
