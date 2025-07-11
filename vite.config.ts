42573aad1f8378f73c19d574bb805556e86881c6
Aulia Mega <uh92t4@inf.elte.hu>
2025-07-11 11:58:07 +0200

    Update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/bold-web-folio/',
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),   // <— alias @ -> /src
+    },
+  },
 });
-
